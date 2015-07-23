6474e18032492d3b3610ba244ae6367d104a0db2
terproc <roc423@gmail.com>
2015-07-22 21:11:40 -0400

    Updated the functions in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions to allow for the caching of an inverse matrix by storing
+## that inverse matrix in a "special matrix" created by the
+## makeCacheMatrix function
 
-## Write a short comment describing this function
+## Function to cache the inverse of a matrix; gets and sets the given
+## matrix, and gets and sets the inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(cMatrix = matrix()) {
+    iMatrix <- NULL
 
+    set <- function(givenMatrix){
+        cMatrix <<- givenMatrix
+        iMatrix <<- NULL
+    }
+
+    get <- function() cMatrix
+
+    setIMatrix <- function(invMatrix) iMatrix <<- invMatrix
+
+    getIMatrix <- function() iMatrix
+
+    list(set = set, get = get, setIMatrix = setIMatrix, getIMatrix = getIMatrix)
 }
 
 
-## Write a short comment describing this function
+## Checks to see if given "special matrix" has a cached inverse matrix;
+## if it does, returns that matrix, if not, it calculates the inverse,
+## stores it in the "special matrix" and returns the inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    invMatrix <- x$getIMatrix()
+
+    if(!is.null(invMatrix)){
+        message("Retrieving cached inverse matrix")
+        return(invMatrix)
+    }
+    invMatrix <- solve(x$get())
+    x$setIMatrix(invMatrix)
+    invMatrix
 }
